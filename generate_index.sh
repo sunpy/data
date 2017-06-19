c817f5969d694f3bb7bc4a29a2b42ae153cd0b74
Stuart Mumford <stuart@cadair.com>
2017-06-19 14:29:51 +0100

    Automatically generate the index.html with a list of files

---
+++ generate_index.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+root="./"
+echo "<html>
+    <head>
+        <title>SunPy Data Store</title>
+    </head>
+    <body>
+        <p> SunPy Data Repo, see <a href=https://github.com/sunpy/sample-data>GitHub</a> for sources.
+        <h2> File Listing</h2>
+
+        "
+echo "<ul>"
+for file in "$root"/*/*; do
+  parentpath="${file#*/}"
+  parent="${parentpath%/*}"
+  filename="${file##*/}"
+  if [[ -z $oldparent ]]; then
+    echo "  <li> $parent </li>" && oldparent="$parent"
+    echo "  <ul>"
+  elif [[ $oldparent != $parent ]]; then
+    echo "  </ul>"
+    echo "  <li> $parent </li>" && oldparent="$parent"
+    echo "  <ul>"
+  fi
+  echo "    <li><a href=\"$parentpath\">$filename</a></li>"
+done
+echo "  </ul>"
+echo "</ul>" 
+echo "</body>"
+echo "</html>"
