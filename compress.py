a0ad57c9908833b86ca169e04ebe76ee86140fef
Nabil Freij <nabil.freij@gmail.com>
2022-06-07 15:12:22 -0700

    shrunk

--- compress.py
+++ compress.py
@@ -1,4 +1,17 @@
 def compress(file, factor):
+    """
+    Will compress a given ``file`` with a factor of ``factor``.
+
+    Only created for 2D images.
+
+    Parameters
+    ----------
+    file : `str`
+        File to compress.
+    factor : `float`
+        Factor to shrink by.
+        E.g., SDO 4k to 1k, factor = 0.25
+    """
     from astropy.io import fits
     from scipy.ndimage import zoom
 
@@ -7,13 +20,11 @@ def compress(file, factor):
             if isinstance(hdu, fits.hdu.table.TableHDU):
                 continue
             hdu.verify("fix")
-            if "NAXIS3" in hdu.header:
-                data = []
-                for i in range(hdu.data.shape[0]):
-                    data.append(zoom(hdu.data[i], factor, order=0))
+            if hdu.header["NAXIS"] == 2:
+                data = zoom(hdu.data, factor, order=0)
                 hdu.data = data
-                hdu.header["NAXIS1"] = hdu.data.shape[2]
-                hdu.header["NAXIS2"] = hdu.data.shape[1]
-                hdu.header["NAXIS3"] = hdu.data.shape[0]
+                hdu.header["NAXIS1"] = hdu.data.shape[1]
+                hdu.header["NAXIS2"] = hdu.data.shape[0]
             hdu = fits.CompImageHDU(hdu.data, hdu.header)
-        hdus.writeto(f"{file}.fits")
+        # Avoid overwriting the original file, you will need to rename after the fact.
+        hdus.writeto(f"{file}.fits", overwrite=True)
