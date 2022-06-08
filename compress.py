36f6c3eedb8b92a3b89f72a9a30d96b8562ac96e
Nabil Freij <nabil.freij@gmail.com>
2022-06-08 09:35:14 -0700

    fix file probs

--- compress.py
+++ compress.py
@@ -1,30 +1,24 @@
-def compress(file, factor):
+def compress(file, dims):
     """
     Will compress a given ``file`` with a factor of ``factor``.
 
-    Only created for 2D images.
+    Will only work for files that sunpy.map.Map can read.
 
     Parameters
     ----------
     file : `str`
         File to compress.
-    factor : `float`
-        Factor to shrink by.
-        E.g., SDO 4k to 1k, factor = 0.25
+    dims : `float`
+        Final dimensions of the compressed image in pixels.
     """
+    import astropy.units as u
     from astropy.io import fits
-    from scipy.ndimage import zoom
 
-    with fits.open(file) as hdus:
-        for hdu in hdus:
-            if isinstance(hdu, fits.hdu.table.TableHDU):
-                continue
-            hdu.verify("fix")
-            if hdu.header["NAXIS"] == 2:
-                data = zoom(hdu.data, factor, order=0)
-                hdu.data = data
-                hdu.header["NAXIS1"] = hdu.data.shape[1]
-                hdu.header["NAXIS2"] = hdu.data.shape[0]
-            hdu = fits.CompImageHDU(hdu.data, hdu.header)
-        # Avoid overwriting the original file, you will need to rename after the fact.
-        hdus.writeto(f"{file}.fits", overwrite=True)
+    from sunpy.map import Map
+
+    amap = Map(file)
+    new_dimensions = [dims, dims] * u.pixel
+    resampled_map = amap.resample(new_dimensions)
+    # Write the resampled map to a new file.
+    # You will want to move the file to the correct location later on.
+    resampled_map.save(f"{file}.resample.fits", hdu_type=fits.CompImageHDU)
